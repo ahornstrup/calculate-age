56b97b321f39f5fa40efc3d3573d944e77219b37
ahornstrup <eaa24ahor@students.eaaa.dk>
2025-01-25 13:58:13 +0100

    opgave klaret :)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+const yearInput = prompt("Indtast dit fødselsår");
+
+const year = parseInt(yearInput, 10);
+
+const currentYear = new Date('2025').getFullYear();
+
+const age = currentYear - year;
+
+console.log(`Du bliver ${age} år gammel i 2025.`);
